e07b49e99473b55eb330cd41fa551efb6eb44d6d
Kevin0115 <kevin.choi0115@gmail.com>
2019-01-19 23:46:53 -0800

    Updates

---
+++ precache-manifest.e7a7ef591d2813cf31a5ec6e3e2e5aa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "250125365fbb9734b954",
+    "url": "/shopify-web-challenge-2019/static/js/runtime~main.25012536.js"
+  },
+  {
+    "revision": "b1a078ee1ccc8142c396",
+    "url": "/shopify-web-challenge-2019/static/js/main.b1a078ee.chunk.js"
+  },
+  {
+    "revision": "7c80b74f553b7efb969f",
+    "url": "/shopify-web-challenge-2019/static/js/1.7c80b74f.chunk.js"
+  },
+  {
+    "revision": "b1a078ee1ccc8142c396",
+    "url": "/shopify-web-challenge-2019/static/css/main.1dd0d567.chunk.css"
+  },
+  {
+    "revision": "0caea8770ca43a0f2b3f0ef2846af628",
+    "url": "/shopify-web-challenge-2019/index.html"
+  }
+];
